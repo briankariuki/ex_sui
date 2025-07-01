ee0452c9b24f6b72c0f79cc8d12763564bde89c4
briankariuki <briankariukidev@gmail.com>
2025-07-02 01:20:18 +0300

    add reference gas price

--- lib/ex_sui.ex
+++ lib/ex_sui.ex
@@ -23,6 +23,192 @@ defmodule ExSui do
   alias Sui.Rpc.V2alpha.GetBalanceRequest
   alias Sui.Rpc.V2alpha.GetBalanceResponse
 
+  alias Sui.Rpc.V2beta.Epoch
+  alias Sui.Rpc.V2beta.GetEpochRequest
+
+  @doc """
+  Gets the current sui blockchain reference gas price.
+
+  ## Examples
+      iex> ExSui.get_reference_gas_price()
+      {:ok,
+        %Sui.Rpc.V2beta.Epoch{
+          epoch: 0,
+          committee: nil,
+          system_state: %Sui.Rpc.V2beta.SystemState{
+            version: 1,
+            epoch: 0,
+            protocol_version: 86,
+            validators: %Sui.Rpc.V2beta.ValidatorSet{
+              total_stake: 20000000000000000,
+              active_validators: [
+                %Sui.Rpc.V2beta.Validator{
+                  name: "validator-0",
+                  address: "0x1eb1af5dbe9a1aa92d4e5204de368b3ce7bf4882ad6d8bb85f48b42449d72d38",
+                  description: "",
+                  image_url: "",
+                  project_url: "",
+                  protocol_public_key: <<153, 242, 94, 246, 31, 128, 50, 185, 20, 99,
+                    100, 96, 152, 44, 92, 198, 241, 52, 239, 29, 218, 231, 102, 87,
+                    242, 203, 254, 193, 235, 252, 141, 9, ...>>,
+                  proof_of_possession: <<130, 12, 205, 55, 195, 3, 38, 147, 167, 184,
+                    109, 227, 179, 83, 130, 17, 58, 234, 241, 143, 75, 122, 82, 62, 5,
+                    30, 234, 200, 169, 237, 81, ...>>,
+                  network_public_key: <<216, 91, 244, 132, 238, 159, 32, 111, 72, 51,
+                    40, 184, 70, 31, 22, 64, 209, 252, 204, 224, 48, 5, 177, 155, 130,
+                    199, 49, 88, 13, 125, ...>>,
+                  worker_public_key: <<10, 141, 144, 37, 214, 167, 170, 235, 223, 11,
+                    240, 221, 114, 162, 57, 6, 45, 207, 109, 129, 25, 95, 148, 144,
+                    205, 238, 6, 170, 13, ...>>,
+                  network_address: "/ip4/127.0.0.1/tcp/40655/http",
+                  p2p_address: "/ip4/127.0.0.1/udp/39933",
+                  primary_address: "/ip4/127.0.0.1/udp/40709",
+                  worker_address: "/ip4/127.0.0.1/udp/32899",
+                  next_epoch_protocol_public_key: nil,
+                  next_epoch_proof_of_possession: nil,
+                  next_epoch_network_public_key: nil,
+                  next_epoch_worker_public_key: nil,
+                  next_epoch_network_address: nil,
+                  next_epoch_p2p_address: nil,
+                  next_epoch_primary_address: nil,
+                  next_epoch_worker_address: nil,
+                  metadata_extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                    id: "0x7a4f34bd975a287b1a22e3f49634ad97cd93b66077eda74d7eef4badadff9e50",
+                    size: 0,
+                    __unknown_fields__: []
+                  },
+                  voting_power: 10000,
+                  operation_cap_id: "0x5190d89c0182783d1190f79414392248d3baf6013abaae28bfd487dff671d686",
+                  gas_price: 1000,
+                  staking_pool: %Sui.Rpc.V2beta.StakingPool{
+                    id: "0x6947ccf15e784f0b4825c1b4c8745de654ba303b8a5b4e317da929a889747c75",
+                    activation_epoch: 0,
+                    deactivation_epoch: nil,
+                    sui_balance: 20000000000000000,
+                    rewards_pool: 0,
+                    pool_token_balance: 20000000000000000,
+                    exchange_rates: %Sui.Rpc.V2beta.MoveTable{
+                      id: "0x57d93973c930da83daf0b7981bbaf635ae1f83d4e002c41b3ed6827a79e9c94a",
+                      size: 1,
+                      __unknown_fields__: []
+                    },
+                    pending_stake: 0,
+                    pending_total_sui_withdraw: 0,
+                    pending_pool_token_withdraw: 0,
+                    extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                      id: "0xb7a754589ff16d0782afeab0cd54b086592e231c474fcbe67143bef15331c5be",
+                      ...
+                    },
+                    __unknown_fields__: []
+                  },
+                  commission_rate: 200,
+                  next_epoch_stake: 20000000000000000,
+                  next_epoch_gas_price: 1000,
+                  next_epoch_commission_rate: 200,
+                  extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                    id: "0x402ce6623bb3344ddf08a665f9e1f4952837d864d17c84d163dc94ce74262f55",
+                    size: 0,
+                    __unknown_fields__: []
+                  },
+                  __unknown_fields__: []
+                }
+              ],
+              pending_active_validators: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x296d3df678e6b98d1d1073fb0e441e4557da4035700ffc50f961a2d68dc1f195",
+                size: 0,
+                __unknown_fields__: []
+              },
+              pending_removals: [],
+              staking_pool_mappings: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x1fcda91245fdc9df03656a737e8cbd1d3a42cf21759f97eb4c2e2af0b7cff1ec",
+                size: 1,
+                __unknown_fields__: []
+              },
+              inactive_validators: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x32ac2de296e73af71aee47930e2d93bff4c6f059caa2ac17b5161f299e4bee6f",
+                size: 0,
+                __unknown_fields__: []
+              },
+              validator_candidates: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x761487901aefaf0af8f0e9b2673e8bfeb95306051fa93d49ef806271dda6e495",
+                size: 0,
+                __unknown_fields__: []
+              },
+              at_risk_validators: %{},
+              extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                id: "0xe0e644162cc465ad3c49d18699e52baf94c762761824cf82295d119564c5e6da",
+                size: 0,
+                __unknown_fields__: []
+              },
+              __unknown_fields__: []
+            },
+            storage_fund: %Sui.Rpc.V2beta.StorageFund{
+              total_object_storage_rebates: 0,
+              non_refundable_balance: 0,
+              __unknown_fields__: []
+            },
+            parameters: %Sui.Rpc.V2beta.SystemParameters{
+              epoch_duration_ms: 86400000,
+              stake_subsidy_start_epoch: 0,
+              min_validator_count: nil,
+              max_validator_count: 150,
+              min_validator_joining_stake: 30000000000000000,
+              validator_low_stake_threshold: 20000000000000000,
+              validator_very_low_stake_threshold: 15000000000000000,
+              validator_low_stake_grace_period: 7,
+              extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x8276777c562234de7217ace33dced01199d56929b979d435f2149cc043f78ba4",
+                size: 0,
+                __unknown_fields__: []
+              },
+              __unknown_fields__: []
+            },
+            reference_gas_price: 1000,
+            validator_report_records: [],
+            stake_subsidy: %Sui.Rpc.V2beta.StakeSubsidy{
+              balance: 9200000000000000000,
+              distribution_counter: 0,
+              current_distribution_amount: 1000000000000000,
+              stake_subsidy_period_length: 10,
+              stake_subsidy_decrease_rate: 1000,
+              extra_fields: %Sui.Rpc.V2beta.MoveTable{
+                id: "0x14d85b0fa2a2416dfc9580b14ff70aaab1ea6f510e6114b1486ec8d137445383",
+                size: 0,
+                __unknown_fields__: []
+              },
+              __unknown_fields__: []
+            },
+            safe_mode: false,
+            safe_mode_storage_rewards: 0,
+            safe_mode_computation_rewards: 0,
+            safe_mode_storage_rebates: 0,
+            safe_mode_non_refundable_storage_fee: 0,
+            epoch_start_timestamp_ms: 1751401172821,
+            extra_fields: %Sui.Rpc.V2beta.MoveTable{
+              id: "0x11f95c9464fb556f65a168928c426cfb2713d89d3eb4cb24965d9f720ba62342",
+              size: 0,
+              __unknown_fields__: []
+            },
+            __unknown_fields__: []
+          },
+          first_checkpoint: 0,
+          last_checkpoint: nil,
+          start: %Google.Protobuf.Timestamp{
+            seconds: 1751401172,
+            nanos: 821000000,
+            __unknown_fields__: []
+          },
+          end: nil,
+          reference_gas_price: 1000,
+          protocol_config: nil,
+          __unknown_fields__: []
+        }}
+
+  """
+  @spec get_reference_gas_price(GetEpochRequest.t() | nil) ::
+          {:ok, Epoch.t()} | {:error, GRPC.RPCError.t()}
+  defdelegate get_reference_gas_price(request \\ nil), to: ExSui.Core
+
   @doc """
   Gets the current Sui blockchain checkpoint.
 
